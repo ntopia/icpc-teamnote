984b01d3b90f0ad36f56f032cc0280da3059fa48
ntopia <ntopia.p@gmail.com>
2016-08-15 16:24:59 +0900

    add lca

---
+++ src/graph/lca.cpp
@@ -0,0 +1,43 @@
+const int MAXN = 100;
+const int MAXLN = 9;
+vector<int> tree[MAXN];
+int depth[MAXN];
+int par[MAXLN][MAXN];
+
+void dfs(int nod, int parent) {
+    for (int next : tree[nod]) {
+        if (next == parent) continue;
+        depth[next] = depth[nod] + 1;
+        par[0][next] = parent;
+        dfs(next, nod);
+    }
+}
+
+void prepare_lca() {
+    const int root = 0;
+    dfs(root, -1);
+    par[0][root] = root;
+    for (int i = 1; i < MAXLN; ++i)
+        for (int j = 0; j < n; ++j)
+            par[i][j] = par[i - 1][par[i - 1][j]];
+}
+
+// find lowest common ancestor in tree between u & v
+// assumption : must call 'prepare_lca' once before call this
+// O(logV)
+int lca(int u, int v) {
+    if (depth[u] < depth[v]) swap(u, v);
+    if (depth[u] > depth[v]) {
+        for (int i = MAXLN - 1; i >= 0; --i)
+            if (depth[u] - (1 << i) >= depth[v])
+                u = par[i][u];
+    }
+    if (u == v) return u;
+    for (int i = MAXLN - 1; i >= 0; --i) {
+        if (par[i][u] != par[i][v]) {
+            u = par[i][u];
+            v = par[i][v];
+        }
+    }
+    return par[0][u];
+}
