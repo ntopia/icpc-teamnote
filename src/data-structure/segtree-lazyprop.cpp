270f353180114020661c52a85c66e5d8d984b6af
ntopia <ntopia.p@gmail.com>
2016-08-15 16:40:44 +0900

    add segtree with lazy prop

---
+++ src/data-structure/segtree-lazyprop.cpp
@@ -0,0 +1,44 @@
+// example implementation of sum tree
+
+const int TSIZE = 131072; // always 2^k form && n <= TSIZE
+int segtree[TSIZE * 2 + 1], pending[TSIZE * 2 + 1];
+const int EMPTY = 0;
+void seg_init(int nod, int l, int r) {
+    if (l == r) tree[nod] = dat[l];
+    else {
+        int m = (l + r) >> 1;
+        seg_init(nod << 1, l, m);
+        seg_init(nod << 1 | 1, m + 1, r);
+        segtree[nod] = segtree[nod << 1] + segtree[nod << 1 | 1];
+    }
+}
+void seg_relax(int nod, int l, int r) {
+    if (pending[nod] != EMPTY) {
+        segtree[nod] += (r - l + 1) * pending[nod];
+        if (l < r) {
+            pending[nod << 1] += pending[nod];
+            pending[nod << 1 | 1] += pending[nod];
+        }
+        pending[nod] = EMPTY;
+    }
+}
+int seg_query(int nod, int l, int r, int s, int e) {
+    if (r < s || e < l) return 0;
+    seg_relax(nod, l, r);
+    if (s <= l && r <= e) return segtree[nod];
+    int m = (l + r) >> 1;
+    return seg_query(nod << 1, l, m, s, e) + seg_query(nod << 1 | 1, m + 1, r, s, e);
+}
+void seg_update(int nod, int l, int r, int s, int e, int val) {
+    seg_relax(nod, l, r);
+    if (r < s || e < l) return;
+    if (s <= l && r <= e) {
+        pending[nod] += val;
+        seg_relax(nod, l, r);
+        return;
+    }
+    int m = (l + r) >> 1;
+    seg_update(nod << 1, l, m, s, e, val);
+    seg_update(nod << 1 | 1, m + 1, r, s, e, val);
+    segtree[nod] = segtree[nod << 1] + segtree[nod << 1 | 1];
+}
