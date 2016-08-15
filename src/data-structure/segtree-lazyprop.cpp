3a467006f8aa0aad15ded4e6277b66d6b9184772
ntopia <ntopia.p@gmail.com>
2016-08-15 17:16:09 +0900

    modify segtree implementation

--- src/data-structure/segtree-lazyprop.cpp
+++ src/data-structure/segtree-lazyprop.cpp
@@ -1,8 +1,6 @@
 // example implementation of sum tree
-
 const int TSIZE = 131072; // always 2^k form && n <= TSIZE
-int segtree[TSIZE * 2 + 1], pending[TSIZE * 2 + 1];
-const int EMPTY = 0;
+int segtree[TSIZE * 2], prop[TSIZE * 2];
 void seg_init(int nod, int l, int r) {
     if (l == r) segtree[nod] = dat[l];
     else {
@@ -13,32 +11,36 @@ void seg_init(int nod, int l, int r) {
     }
 }
 void seg_relax(int nod, int l, int r) {
-    if (pending[nod] != EMPTY) {
-        segtree[nod] += (r - l + 1) * pending[nod];
-        if (l < r) {
-            pending[nod << 1] += pending[nod];
-            pending[nod << 1 | 1] += pending[nod];
-        }
-        pending[nod] = EMPTY;
+    if (prop[nod] == 0) return;
+    if (l < r) {
+        int m = (l + r) >> 1;
+        segtree[nod << 1] += (m - l + 1) * prop[nod];
+        prop[nod << 1] += prop[nod];
+        segtree[nod << 1 | 1] += (r - m) * prop[nod];
+        prop[nod << 1 | 1] += prop[nod];
     }
+    prop[nod] = 0;
 }
 int seg_query(int nod, int l, int r, int s, int e) {
     if (r < s || e < l) return 0;
-    seg_relax(nod, l, r);
     if (s <= l && r <= e) return segtree[nod];
+    seg_relax(nod, l, r);
     int m = (l + r) >> 1;
     return seg_query(nod << 1, l, m, s, e) + seg_query(nod << 1 | 1, m + 1, r, s, e);
 }
 void seg_update(int nod, int l, int r, int s, int e, int val) {
-    seg_relax(nod, l, r);
     if (r < s || e < l) return;
     if (s <= l && r <= e) {
-        pending[nod] += val;
-        seg_relax(nod, l, r);
+        segtree[nod] += (r - l + 1) * val;
+        prop[nod] += val;
         return;
     }
+    seg_relax(nod, l, r);
     int m = (l + r) >> 1;
     seg_update(nod << 1, l, m, s, e, val);
     seg_update(nod << 1 | 1, m + 1, r, s, e, val);
     segtree[nod] = segtree[nod << 1] + segtree[nod << 1 | 1];
 }
+// usage:
+// seg_update(1, 0, n - 1, qs, qe, val);
+// seg_query(1, 0, n - 1, qs, qe);
