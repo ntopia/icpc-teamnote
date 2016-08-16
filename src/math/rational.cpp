c285f038c67a14a4302c87f9d60132facf43623a
ntopia <ntopia.p@gmail.com>
2016-08-16 19:40:13 +0900

    add rational number class

---
+++ src/math/rational.cpp
@@ -0,0 +1,39 @@
+struct rational {
+    long long p, q;
+
+    void red() {
+        if (q < 0) {
+            p *= -1;
+            q *= -1;
+        }
+        ll t = gcd((p >= 0 ? p : -p), q);
+        p /= t;
+        q /= t;
+    }
+
+    rational(): p(0), q(1) {}
+    rational(long long p_): p(p_), q(1) {}
+    rational(long long p_, long long q_): p(p_), q(q_) { red(); }
+
+    bool operator==(const rational& rhs) const {
+        return p == rhs.p && q == rhs.q;
+    }
+    bool operator!=(const rational& rhs) const {
+        return p != rhs.p || q != rhs.q;
+    }
+    bool operator<(const rational& rhs) const {
+        return p * rhs.q < rhs.p * q;
+    }
+    rational operator+(const rational& rhs) const {
+        return rational(p * rhs.q + q * rhs.p, q * rhs.q);
+    }
+    rational operator-(const rational& rhs) const {
+        return rational(p * rhs.q - q * rhs.p, q * rhs.q);
+    }
+    rational operator*(const rational& rhs) const {
+        return rational(p * rhs.p, q * rhs.q);
+    }
+    rational operator/(const rational& rhs) const {
+        return rational(p * rhs.q, q * rhs.p);
+    }
+};
