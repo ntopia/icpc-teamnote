d80bebf53ae3688d1edfe3cbc54f71fb0814baba
ntopia <ntopia.p@gmail.com>
2016-08-16 21:06:51 +0900

    add tarjan's scc algorithm

---
+++ src/graph/scc-tarjan.cpp
@@ -0,0 +1,38 @@
+const int MAXN = 100;
+vector<int> graph[MAXN];
+int up[MAXN], visit[MAXN], vtime;
+vector<int> stk;
+int scc_idx[MAXN], scc_cnt;
+
+void dfs(int nod) {
+    up[nod] = visit[nod] = ++vtime;
+    stk.push_back(nod);
+    for (int next : graph[nod]) {
+        if (visit[next] == 0) {
+            dfs(next);
+            up[nod] = min(up[nod], up[next]);
+        }
+        else if (scc_idx[next] == 0)
+            up[nod] = min(up[nod], visit[next]);
+    }
+    if (up[nod] == visit[nod]) {
+        ++scc_cnt;
+        int t;
+        do {
+            t = stk.back();
+            stk.pop_back();
+            scc_idx[t] = scc_cnt;
+        } while (!stk.empty() && t != nod);
+    }
+}
+
+// find SCCs in given directed graph
+// O(V+E)
+void get_scc() {
+    vtime = 0;
+    memset(visit, 0, sizeof(visit));
+    scc_cnt = 0;
+    memset(scc_idx, 0, sizeof(scc_idx));
+    for (int i = 0; i < n; ++i)
+        if (visit[i] == 0) dfs(i);
+}
