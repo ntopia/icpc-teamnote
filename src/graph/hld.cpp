1fa7401dbf09ead9acefd0f4bff062b543280daf
Inseob Kim <iskim517@gmail.com>
2016-08-18 03:02:19 +0900

    Add hld

---
+++ src/graph/hld.cpp
@@ -0,0 +1,110 @@
+// heavy-light decomposition
+//
+// hld h;
+// insert edges to tree[0~n-1];
+// h.init(n);
+// h.decompose(root);
+// h.hldquery(u, v); // edges from u to v
+struct hld {
+    static const int MAXLN = 18;
+    static const int MAXN = 1 << (MAXLN - 1);
+    vector<int> tree[MAXN];
+    int subsize[MAXN], depth[MAXN], pa[MAXLN][MAXN];
+
+    int chead[MAXN], cidx[MAXN];
+    int lchain;
+    int flatpos[MAXN + 1], fptr;
+
+    void dfs(int u, int par) {
+        pa[0][u] = par;
+        subsize[u] = 1;
+        for (int v : tree[u]) {
+            if (v == pa[0][u]) continue;
+            depth[v] = depth[u] + 1;
+            dfs(v, u);
+            subsize[u] += subsize[v];
+        }
+    }
+
+    void init(int size)
+    {
+        lchain = fptr = 0;
+        dfs(0, -1);
+        memset(chead, -1, sizeof(chead));
+
+        for (int i = 1; i < MAXLN; i++) {
+            for (int j = 0; j < size; j++) {
+                if (pa[i - 1][j] != -1) {
+                    pa[i][j] = pa[i - 1][pa[i - 1][j]];
+                }
+            }
+        }
+    }
+
+    void decompose(int u) {
+        if (chead[lchain] == -1) chead[lchain] = u;
+        cidx[u] = lchain;
+        flatpos[u] = ++fptr;
+
+        int maxchd = -1;
+        for (int v : tree[u]) {
+            if (v == pa[0][u]) continue;
+            if (maxchd == -1 || subsize[maxchd] < subsize[v]) maxchd = v;
+        }
+        if (maxchd != -1) decompose(maxchd);
+
+        for (int v : tree[u]) {
+            if (v == pa[0][u] || v == maxchd) continue;
+            ++lchain; decompose(v);
+        }
+    }
+
+    int lca(int u, int v) {
+        if (depth[u] < depth[v]) swap(u, v);
+
+        int logu;
+        for (logu = 1; 1 << logu <= depth[u]; logu++);
+        logu--;
+
+        int diff = depth[u] - depth[v];
+        for (int i = logu; i >= 0; --i) {
+            if ((diff >> i) & 1) u = pa[i][u];
+        }
+        if (u == v) return u;
+
+        for (int i = logu; i >= 0; --i) {
+            if (pa[i][u] != pa[i][v]) {
+                u = pa[i][u];
+                v = pa[i][v];
+            }
+        }
+        return pa[0][u];
+    }
+
+    // TODO: implement query functions
+    inline int query(int s, int e) {
+        return 0;
+    }
+
+    int subquery(int u, int v, int t) {
+        int uchain, vchain = cidx[v];
+        int ret = 0;
+        for (;;) {
+            uchain = cidx[u];
+            if (uchain == vchain) {
+                ret += query(flatpos[v], flatpos[u]);
+                break;
+            }
+
+            ret += query(flatpos[chead[uchain]], flatpos[u]);
+            u = pa[0][chead[uchain]];
+        }
+        return ret;
+    }
+
+    inline int hldquery(int u, int v) {
+        int p = lca(u, v);
+        return subquery(u, p) + subquery(v, p) - query(flatpos[p], flatpos[p]);
+    }
+};
+
