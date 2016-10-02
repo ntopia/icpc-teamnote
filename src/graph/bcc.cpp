5b092d1c0c3c47f407804738e43b40307c909969
ntopia <ntopia.p@gmail.com>
2016-10-02 23:22:44 +0900

    fix bcc implementation bug

--- src/graph/bcc.cpp
+++ src/graph/bcc.cpp
@@ -3,9 +3,10 @@ vector<pair<int, int>> graph[MAXN];  // { next vertex id, edge id }
 int up[MAXN], visit[MAXN], vtime;
 vector<int> stk;
 
-vector<int> cut_vertex;
-vector<int> bridge;
-int bcc_idx[MAXN], bcc_cnt;
+vector<int> cut_vertex;     // list of vertex ids
+vector<int> bridge;         // list of edge ids
+vector<int> bcc_idx[MAXN];  // list of bccids for vertex i
+int bcc_cnt;
 
 void dfs(int nod, int par_edge) {
     up[nod] = visit[nod] = ++vtime;
@@ -21,7 +22,7 @@ void dfs(int nod, int par_edge) {
             if (up[next] >= visit[nod]) {
                 ++bcc_cnt;
                 do {
-                    bcc_idx[stk.back()] = bcc_cnt;
+                    bcc_idx[stk.back()].push_back(bcc_cnt);
                     stk.pop_back();
                 } while (!stk.empty() && stk.back() != nod);
                 bcc_idx[nod] = bcc_cnt;
@@ -43,7 +44,7 @@ void get_bcc() {
     memset(visit, 0, sizeof(visit));
     cut_vertex.clear();
     bridge.clear();
-    memset(bcc_idx, 0, sizeof(bcc_idx));
+    for (int i = 0; i < n; ++i) bcc_idx[i].clear();
     bcc_cnt = 0;
     for (int i = 0; i < n; ++i) {
         if (visit[i] == 0)
