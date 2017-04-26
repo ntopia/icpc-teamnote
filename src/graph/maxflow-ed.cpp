9e600990c4519a89f8ddcae072a1817a980bde39
Inseob Kim <iskim517@gmail.com>
2017-04-26 20:54:13 +0900

    Remove redundant vector<edge>

--- src/graph/maxflow-ed.cpp
+++ src/graph/maxflow-ed.cpp
@@ -3,30 +3,23 @@ struct MaxFlowEdgeDemands
     MaxFlowDinic mf;
     using flow_t = MaxFlowDinic::flow_t;
 
-    struct edge {
-        int s, e;
-        flow_t cap, demands;
-    };
-
     vector<flow_t> ind, outd;
-    vector<edge> edges;
     flow_t D; int n;
 
     void init(int _n) {
-        n = _n; D = 0; edges.clear(); mf.init(n + 2);
+        n = _n; D = 0; mf.init(n + 2);
         ind.clear(); outd.clear();
         ind.resize(n, 0); outd.resize(n, 0);
     }
 
     void add_edge(int s, int e, flow_t cap, flow_t demands = 0) {
-        edges.push_back({ s, e, cap, demands });
+		mf.add_edge(s, e, cap - demands);
         D += demands; ind[e] += demands; outd[s] += demands;
     }
 
     // returns { false, 0 } if infeasible
     // { true, maxflow } if feasible
     pair<bool, flow_t> solve(int source, int sink) {
-        for (auto &e : edges) mf.add_edge(e.s, e.e, e.cap - e.demands);
         mf.add_edge(sink, source, numeric_limits<flow_t>::max());
 
         for (int i = 0; i < n; i++) {
