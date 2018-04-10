e708a2d60f4c72dab561e01b96fab63e2a933021
ntopia <ntopia.p@gmail.com>
2018-04-11 03:00:07 +0900

    Change implementation of BCC
    - BCC를 이루는 정점들을 찾는 구현이었는데 간선들을 찾는 구현으로 변경함
    - 버그 1개 고침

--- src/graph/bcc.cpp
+++ src/graph/bcc.cpp
@@ -1,39 +1,40 @@
 const int MAXN = 100;
 vector<pair<int, int>> graph[MAXN];  // { next vertex id, edge id }
 int up[MAXN], visit[MAXN], vtime;
-vector<pair<int, int>> stk;
+vector<int> stk;
 
-int is_cut[MAXN];           // v is cut vertex if  is_cut[v] > 0
-vector<int> bridge;         // list of edge ids
-vector<int> bcc_idx[MAXN];  // list of bccids for vertex i
+int is_cut[MAXN];             // v is cut vertex if  is_cut[v] > 0
+vector<int> bridge;           // list of edge ids
+vector<int> bcc_edges[MAXN];  // list of edge ids in a bcc
 int bcc_cnt;
 
 void dfs(int nod, int par_edge) {
     up[nod] = visit[nod] = ++vtime;
     int child = 0;
     for (const auto& e : graph[nod]) {
-        int next = e.first, edge_id = e.second;
-        if (edge_id == par_edge) continue;
+        int next = e.first, eid = e.second;
+        if (eid == par_edge) continue;
         if (visit[next] == 0) {
-            stk.push_back({ nod, next });
+            stk.push_back(eid);
             ++child;
-            dfs(next, edge_id);
-            if (up[next] == visit[next]) bridge.push_back(edge_id);
+            dfs(next, eid);
+            if (up[next] == visit[next]) bridge.push_back(eid);
             if (up[next] >= visit[nod]) {
                 ++bcc_cnt;
                 do {
-                    auto last = stk.back();
+                    auto lasteid = stk.back();
                     stk.pop_back();
-                    bcc_idx[last.second].push_back(bcc_cnt);
-                    if (last == pair<int, int>{ nod, next }) break;
+                    bcc_edges[bcc_cnt].push_back(lasteid);
+                    if (lasteid == eid) break;
                 } while (!stk.empty());
-                bcc_idx[nod].push_back(bcc_cnt);
                 is_cut[nod]++;
             }
             up[nod] = min(up[nod], up[next]);
         }
-        else
+        else if (visit[next] < visit[nod]) {
+            stk.push_back(eid);
             up[nod] = min(up[nod], visit[next]);
+        }
     }
     if (par_edge == -1 && is_cut[nod] == 1)
         is_cut[nod] = 0;
@@ -46,7 +47,7 @@ void get_bcc() {
     memset(visit, 0, sizeof(visit));
     memset(is_cut, 0, sizeof(is_cut));
     bridge.clear();
-    for (int i = 0; i < n; ++i) bcc_idx[i].clear();
+    for (int i = 0; i < n; ++i) bcc_edges[i].clear();
     bcc_cnt = 0;
     for (int i = 0; i < n; ++i) {
         if (visit[i] == 0)
